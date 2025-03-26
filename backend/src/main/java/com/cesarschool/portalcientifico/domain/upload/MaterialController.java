9d64a0a02885fceec9c845ebca91f1581620b472
ibiapleo <leonardojose159357@gmail.com>
2025-03-26 19:08:32 -0300

    feature: implementar metodos para obter os materiais

--- backend/src/main/java/com/cesarschool/portalcientifico/domain/upload/MaterialController.java
+++ backend/src/main/java/com/cesarschool/portalcientifico/domain/upload/MaterialController.java
@@ -1,6 +1,7 @@
 package com.cesarschool.portalcientifico.domain.upload;
 
 import com.cesarschool.portalcientifico.domain.user.User;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/materials")
@@ -24,5 +26,23 @@ public class MaterialController {
         User user = (User) authentication.getPrincipal();
         MaterialResponseDTO materialResponse = materialService.uploadMaterial(materialRequestDTO, user, file);
         return ResponseEntity.status(HttpStatus.CREATED).body(materialResponse);
+    }
+    @Operation(
+            summary = "Obtém um material pelo ID",
+            description = "Retorna os detalhes de um material pelo ID"
+    )
+    @GetMapping("/{id}")
+    public MaterialResponseDTO getMaterialDetails(@PathVariable Long id) {
+        return materialService.getMaterialDetails(id);
+    }
+
+    @Operation(
+            summary = "Obtém todos os materiais",
+            description = "Retorna os detalhes de todos os material"
+    )
+    @GetMapping
+    public ResponseEntity<List<MaterialResponseDTO>> getAllMaterials() {
+        List<MaterialResponseDTO> materials = materialService.getAllMaterials();
+        return ResponseEntity.ok(materials);
     }
 }
--- backend/src/main/java/com/cesarschool/portalcientifico/domain/upload/MaterialRepository.java
+++ backend/src/main/java/com/cesarschool/portalcientifico/domain/upload/MaterialRepository.java
@@ -2,6 +2,4 @@ package com.cesarschool.portalcientifico.domain.upload;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MaterialRepository extends JpaRepository<Material, String> {
-
-}
+public interface MaterialRepository extends JpaRepository<Material, Long> { }
