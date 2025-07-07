f22f76964ac191503404398682f30b4489272f49
Leonardo Ibiapina <161875822+ibiapleo@users.noreply.github.com>
2025-07-07 12:28:31 -0300

    Update vite.config.ts to deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/Portal-Cientifico",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
